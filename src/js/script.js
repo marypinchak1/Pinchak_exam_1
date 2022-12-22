43e9341cf4db208d2a6f019d4384945f4b5fa604
Mariia Pinchak <92802080+marypinchak1@users.noreply.github.com>
2022-12-23 00:10:13 +0200

    add- burger

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,3 @@
-let slider = $(document).ready(function () {
-  $('.your-class').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: true,
-    dots: true,
-    infinite: true
-  });
-});
 
 $('.header__burger').on('click', function(){
   $('.header__burger').toggleClass('active');
